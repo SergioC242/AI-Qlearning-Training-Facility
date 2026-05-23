1e44bd1c8d6e0e482b3f8aa38e95fe7f32277455
SergioC242 <Scroche@ucm.es>
2026-05-23 20:35:55 +0200

    cmake stuuf + primer try

---
+++ build/src/CMakeFiles/AIPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AIPlayer.dir/AIPlayer.cpp.o"
+  "CMakeFiles/AIPlayer.dir/AIPlayer.cpp.o.d"
+  "libAIPlayer.a"
+  "libAIPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AIPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/combat_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/combat_system"
+  "../bin/combat_system.pdb"
+  "CMakeFiles/combat_system.dir/main.cpp.o"
+  "CMakeFiles/combat_system.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/combat_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
